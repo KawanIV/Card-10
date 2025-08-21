fda467e3f25f661dea1e96ea3f11b513251ba154
Kawan Machado <kawan.antonio.machado@gmail.com>
2025-08-21 01:23:02 -0300

    Atualizações
    Mais um tópico finalizado e outro em andamento.

---
+++ Codigos/Aula_Using_Outliers.py
@@ -0,0 +1,45 @@
+#%matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+# dataset
+incomes = np.random.normal(27000, 15000,10000)
+incomes = np.append(incomes,[1000000000])
+
+# gráfico
+plt.hist(incomes,50)
+plt.show()
+
+# anotação
+print("Observe como um valor grande distroce o gráfico...")
+print('-----------------'*7,'\n')
+time.sleep(3)
+
+print('Média de rendimento: ',round(incomes.mean(),2),'\n')
+print("Observe como o valor da média é extremamente alto, desse forma, ele não reflete a média de rendimento das pessoas.")
+print('-----------------'*7,'\n')
+time.sleep(3)
+
+print("Agora aplicaremos uma técnica para filtrar os bilionários para fora do cálculo","\n")
+time.sleep(3)
+
+def reject_outliers(data):
+	u = np.median(data)
+	s = np.std(data)
+	filtered = [e for e in data if (u - 2 * s < e < u + 2 * s)]
+	return filtered
+	
+filtered = reject_outliers(incomes)
+
+plt.hist(filtered, 50)
+plt.show()
+
+print('-----------------'*7,'\n')
+time.sleep(5)
+
+print("Recalculamos a média...",'\n')
+time.sleep(5)
+print("Média recalculada: ",round(np.mean(filtered),2),"\n")
+print("Agora o valor da média reflete de forma mais fiel a mpedia de rendimentos das pessoas...","\n")
+print('-----------------'*7,'\n')
--- Codigos/Card_10_Aula.py
+++ Codigos/Card_10_Aula.py
@@ -2,4 +2,5 @@ import Aula_Using_KNN as knn
 import Aula_Using_PCA as pca
 import Aula_Using_Gym_4 as gym
 import Aula_Using_KFOLD as kfold
-print(f'{knn}\n{pca}\n{gym}\n{kfold}')
+import Aula_Using_Cleaning as cleaning
+print(f'{knn}\n{pca}\n{gym}\n{kfold}\n{cleaning}')
